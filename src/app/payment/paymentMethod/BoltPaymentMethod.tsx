cc40ffafc9002a028a4ba382c52eb1b98d2f1928
FranciscoOnt <fontiveros@intersysconsulting.com>
2020-09-01 16:11:50 -0500

    fix(checkout): INT-3116 Hide Bolt on checkout

--- src/app/payment/paymentMethod/BoltPaymentMethod.tsx
+++ src/app/payment/paymentMethod/BoltPaymentMethod.tsx
@@ -1,23 +1,49 @@
-import React, { useCallback, FunctionComponent } from 'react';
+import React, { useCallback, useContext, FunctionComponent } from 'react';
+
+import PaymentContext from '../PaymentContext';
 
 import HostedPaymentMethod, { HostedPaymentMethodProps } from './HostedPaymentMethod';
+import OfflinePaymentMethod from './OfflinePaymentMethod';
 
 const BoltPaymentMethod: FunctionComponent<HostedPaymentMethodProps> = ({
     initializePayment,
+    method,
     ...rest
 }) => {
-    const initializeBoltPayment = useCallback(options => initializePayment(
-      {
-          ...options,
-          bolt: {
-              useClientScript: true,
-          },
-      }
-  ), [initializePayment]);
+    const paymentContext = useContext(PaymentContext);
+    const showInCheckout = method.initializationData && method.initializationData.showInCheckout;
+
+    const initializeBoltPayment = useCallback(options => initializePayment({
+        ...options,
+        bolt: {
+            useClientScript: true,
+        },
+    }), [initializePayment]);
+
+    const disableMethod = (() => {
+        if (paymentContext) {
+            paymentContext.disableSubmit(method, true);
+        }
+    });
+
+    if (!showInCheckout) {
+        const methodPlaceholder = {
+            ...method,
+            id: '',
+        };
+
+        return <OfflinePaymentMethod
+            { ...rest }
+            initializePayment={ initializePayment }
+            method = { methodPlaceholder }
+            onUnhandledError= { disableMethod }
+        />;
+    }
 
     return <HostedPaymentMethod
         { ...rest }
         initializePayment={ initializeBoltPayment }
+        method={ method }
     />;
 };
 
