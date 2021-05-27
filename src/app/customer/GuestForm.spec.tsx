7d597813eed590152217de2445429831049ff8bd
FranciscoOnt <fontiveros@intersysconsulting.com>
2021-05-27 17:09:01 -0500

    fix(customer): INT-3604 Disable continue as guest button when loading

--- src/app/customer/GuestForm.spec.tsx
+++ src/app/customer/GuestForm.spec.tsx
@@ -17,6 +17,7 @@ describe('GuestForm', () => {
             canSubscribe: true,
             defaultShouldSubscribe: false,
             isContinuingAsGuest: false,
+            isLoading: false,
             onChangeEmail: jest.fn(),
             onContinueAsGuest: jest.fn(),
             onShowLogin: jest.fn(),
@@ -82,6 +83,20 @@ describe('GuestForm', () => {
                 privacyPolicy: false,
                 shouldSubscribe: true,
             });
+    });
+
+    it('disables "continue as guest" button when isLoading is true', () => {
+        const handleContinueAsGuest = jest.fn();
+        const component = mount(
+            <TestComponent
+                isLoading={ true }
+                onContinueAsGuest={ handleContinueAsGuest }
+            />
+        );
+
+        const button = component.find('[data-test="customer-continue-as-guest-button"]');
+
+        expect(button.prop('disabled')).toBeTruthy();
     });
 
     it('displays error message if email is not valid', () => {
@@ -241,5 +256,15 @@ describe('GuestForm', () => {
 
         expect(component.find('[data-test="privacy-policy-field-error-message"]').text())
             .toEqual('Please agree to the Privacy Policy.');
+    });
+
+    it('does not render "sign in" button when loading', () => {
+        const component = mount(
+            <TestComponent
+                isLoading={ true }
+            />
+        );
+
+        expect(component.find('[data-test="customer-continue-button"]').length).toEqual(0);
     });
 });
